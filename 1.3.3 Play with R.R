18b6aa74c1e79950a7d98ec518f2e2194e22461b
sonic694 <mite_waxwork0f@icloud.com>
2025-06-04 14:45:05 +1000

    Add files via upload

---
+++ 1.3.3 Play with R.R
@@ -0,0 +1,24 @@
+#' Title: 1.3.3 Play with R
+#' Author: 
+#' Date: 
+#' Inputs:  None
+#' Outputs: None
+#' Purpose: Learn about R through the swirl package
+#' Notes:  
+
+
+## Run swirl
+# Swirl is already installed in Vocareum. 
+# Copy the code below to the prompt in the console pane.
+# Run the code using Ctrl + Enter (or Cmd + Enter for Mac users). 
+# Follow the instructions to work your way through the tutorial. 
+
+library(swirl) 
+
+# These next two lines are just to make swirl work in Vocareum. 
+# Don't worry about what they are doing. 
+swirl_options(swirl_courses_dir = file.path("swirl_courses"))
+swirl_options(swirl_data_dir = file.path("swirl_user_data"))
+
+
+swirl()
